078a27e279a8077e68754b641e3bec98500b4e1d
eggyo <eggyoyo4859@gmail.com>
2016-09-06 17:40:00 +0700

    v.1

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,9 @@
+var WooCommerce = require('woocommerce');
+var wooCommerce = new WooCommerce({
+  url: 'http://klangsang-led.com',
+  consumerKey: 'ck_e95a3d1d2224538dfe42ac8fd84b48a408f5d2d1',
+  secret: 'cs_366e3a3766c2b7e389b0085ce87a2dc369448d11'
+});
 
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
