d2891a46ca0f2d22011809e6a25934a730808b6e
eggyo <eggyoyo4859@gmail.com>
2016-09-06 17:41:51 +0700

    v.1

--- cloud/main.js
+++ cloud/main.js
@@ -12,7 +12,7 @@ Parse.Cloud.define('hello', function(req, res) {
 
 Parse.Cloud.define('postProducts', function(req, res) {
   var data = req.params.data;
-  WooCommerce.post('products', data, function(err, data, res) {
+  wooCommerce.post('products', data, function(err, data, res) {
     console.log(res);
     res.success("create product done");
   });
