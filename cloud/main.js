59ae1e49e32c2c71a811f278deba9684b2c8a23a
eggyo <eggyoyo4859@gmail.com>
2016-09-06 22:04:15 +0700

    v.1

--- cloud/main.js
+++ cloud/main.js
@@ -10,6 +10,58 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 
+Parse.Cloud.define('testOrder', function(req, response) {
+  var data = {
+  payment_method: 'bacs',
+  payment_method_title: 'Direct Bank Transfer',
+  set_paid: true,
+  billing: {
+    first_name: 'John',
+    last_name: 'Doe',
+    address_1: '969 Market',
+    address_2: '',
+    city: 'San Francisco',
+    state: 'CA',
+    postcode: '94103',
+    country: 'US',
+    email: 'john.doe@example.com',
+    phone: '(555) 555-5555'
+  },
+  shipping: {
+    first_name: 'John',
+    last_name: 'Doe',
+    address_1: '969 Market',
+    address_2: '',
+    city: 'San Francisco',
+    state: 'CA',
+    postcode: '94103',
+    country: 'US'
+  },
+  line_items: [
+    {
+      product_id: 9,
+      quantity: 2
+    },
+    {
+      product_id: 2,
+      quantity: 1
+    }
+  ],
+  shipping_lines: [
+    {
+      method_id: 'flat_rate',
+      method_title: 'Flat Rate',
+      total: 10
+    }
+  ]
+};
+
+WooCommerce.post('orders', data, function(err, data, res) {
+  console.log(res);
+  response.success(data);
+
+  });
+});
 
 Parse.Cloud.define('postProducts', function(req, response) {
   var reqData = req.params.data;
