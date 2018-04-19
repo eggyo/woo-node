42bf4ec6470fdb2888dac09fb91d9127a25b7ccb
eggyo <eggyoyo4859@gmail.com>
2018-04-19 14:43:19 +0700

    v.1

--- index.js
+++ index.js
@@ -7,22 +7,26 @@ var path = require('path');
 var ParseDashboard = require('parse-dashboard');
 var allowInsecureHTTP = true;
 var request = require('request');
+var WooCommerceAPI = require('woocommerce-api');
 
+var WooCommerce = new WooCommerceAPI({
+  url: 'http://klangsang-led.com',
+  wpAPI: true,
+  version: 'wc/v1',
+  consumerKey: 'ck_e95a3d1d2224538dfe42ac8fd84b48a408f5d2d1',
+  consumerSecret: 'cs_366e3a3766c2b7e389b0085ce87a2dc369448d11'
+});
 var dashboard = new ParseDashboard({
-  "apps": [
-    {
-      "serverURL": "http://woo-node.herokuapp.com/parse",
-      "appId": "myAppId",
-      "masterKey": "myMasterKey",
-      "appName": "woo-node"
-    }
-  ],
-  "users": [
-    {
-      "user":"admin",
-      "pass":"pass"
-    }
-    ]
+  "apps": [{
+    "serverURL": "http://woo-node.herokuapp.com/parse",
+    "appId": "myAppId",
+    "masterKey": "myMasterKey",
+    "appName": "woo-node"
+  }],
+  "users": [{
+    "user": "admin",
+    "pass": "pass"
+  }]
 }, allowInsecureHTTP);
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
@@ -37,22 +41,21 @@ var api = new ParseServer({
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   restAPIKey: process.env.REST_KEY,
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   },
   push: {
-    ios: [
-      {
-        pfx: './pushCertificates.dev.p12', // Dev PFX or P12
-        bundleId: 'Eggyo.woo-client',
-        production: false // Dev
-      },{
-        pfx: './pushCertificates.dis.p12', // Prod PFX or P12
-        bundleId: 'Eggyo.woo-client',
-        production: true // Prod
-        }]
-      }
+    ios: [{
+      pfx: './pushCertificates.dev.p12', // Dev PFX or P12
+      bundleId: 'Eggyo.woo-client',
+      production: false // Dev
+    }, {
+      pfx: './pushCertificates.dis.p12', // Prod PFX or P12
+      bundleId: 'Eggyo.woo-client',
+      production: true // Prod
+    }]
+  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -72,15 +75,17 @@ app.use(mountPath, api);
 app.get('/', function(req, res) {
   res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
 });
-app.post('/createdOrderCallback', function(request, response){
-  console.log("createdOrderCallback:"+JSON.stringify(request));      // your JSON
+app.post('/createdOrderCallback', function(request, response) {
+  WooCommerce.get('webhooks', function(err, data, res) {
+    console.log(res);
+  });
 
-  callLineNof(function(response){
+  callLineNof(function(response) {
 
   });
-  Parse.Cloud.run('createdOrderNofPub', { }).then(function(obj) {
+  Parse.Cloud.run('createdOrderNofPub', {}).then(function(obj) {
     response.send(obj);
-    console.log(obj);      // your JSON
+    console.log(obj); // your JSON
     // echo the result back
   });
 });
@@ -100,7 +105,9 @@ function callLineNof(responseMsg) {
       'X-Parse-Application-Id': 'myAppId',
       'X-Parse-REST-API-Key': 'myRestKey'
     },
-    body: {'message':'มีลูกค้าสั่งของ'}
+    body: {
+      'message': 'มีลูกค้าสั่งของ'
+    }
   };
 
   function callback(error, response, body) {
@@ -121,7 +128,7 @@ function callLineNof(responseMsg) {
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
-    console.log('parse-server-example running on port ' + port + '.');
+  console.log('parse-server-example running on port ' + port + '.');
 });
 
 // This will enable the Live Query real-time server
